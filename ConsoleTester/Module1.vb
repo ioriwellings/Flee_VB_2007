0be1e3f3a202958d386fdad66c5f11b6f7fbf2be
ioriwellings <wellingsok@hotmail.com>
2018-04-13 13:28:38 +0800

    .

--- ConsoleTester/Module1.vb
+++ ConsoleTester/Module1.vb
@@ -18,7 +18,7 @@ Module Module1
 		context.Variables.Add("a", 100D)
 		context.Variables.Add("b", 3.14)
 
-        Dim e As IDynamicExpression = context.CompileDynamic("sqrt(4)+if(1>0, if(1=0,10,100), -1)")
+        Dim e As IDynamicExpression = context.CompileDynamic("sqrt(4)+if(if(1=0,true,false), if(1=0,10,100), -1)")
         'Dim e As IDynamicExpression = context.CompileDynamic("1=1 or (1=1 And (1>0 Or 2>0 Or 3>0))")
 		'Dim e As IGenericExpression(Of Double) = context.CompileGeneric(Of Double)("a")
 		Dim result As Object = e.Evaluate()
